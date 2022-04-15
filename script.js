9c7ad6613ff401ba326f4ff3fb2d4af9adc8afa4
David Bergmann <david@netsupply.com>
2022-04-15 12:39:45 +0700

    Gave the button a clear function and lets the user input a different grid size

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 
 const container = document.getElementById("container");
 
+
+const clear = document.querySelector('#clear');
+
+
+function draw(conCells){
+    conCells.forEach((cell) => {
+        cell.addEventListener('mouseover', () => {
+            cell.setAttribute('style', 'background: black;');
+        });
+    });
+}
+
+
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
@@ -10,13 +23,40 @@ function makeRows(rows, cols) {
     };
 };
 
-
 makeRows(16, 16);
 
-const containerCells = document.querySelectorAll('.grid-item')
 
-containerCells.forEach((cell) => {
-    cell.addEventListener('mouseover', () => {
-        cell.setAttribute('style', 'background: black;');
+
+
+
+const containerCells = document.querySelectorAll('.grid-item');
+
+draw(containerCells)
+
+
+clear.addEventListener('click', () => {
+    containerCells.forEach((cell) => {
+        cell.setAttribute('style', 'background: white;')
     });
+
+    container.innerHTML = ""
+    
+
+    let gridChoice = prompt("Enter amount of squares per side for new grid under 100: ");
+    while (gridChoice > 64) {
+        gridChoice = prompt("Enter amount of squares per side for new grid under 100: ");
+    }
+    if (gridChoice === "") {
+        makeRows(16, 16);
+        const ReContainerCells = document.querySelectorAll('.grid-item');
+
+        draw(ReContainerCells);
+    }
+    else {
+        let intGridChoice = Number(gridChoice);
+        makeRows(intGridChoice, intGridChoice);
+        const ReContainerCells = document.querySelectorAll('.grid-item');
+
+        draw(ReContainerCells);
+    }
 });
